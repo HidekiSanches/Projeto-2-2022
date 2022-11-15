d50700497e45e9a4e1fd9743b4d92524143f8dff
HidekiSanches <thiago.sanches@sptech.school>
2022-11-15 12:07:06 -0300

    nova interação com o usuario

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "dnd",
-    user: "root",
-    password: "bonocachorro10",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
